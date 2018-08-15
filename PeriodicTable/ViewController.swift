1b556c4c52b7e6f87b37acd35aa7a5851da5a335
Alexander Meinecke <alexandermeinecke@web.de>
2018-08-15 14:12:39 +0200

    Initial Commit

---
+++ PeriodicTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PeriodicTable
+//
+//  Created by Alexander Meinecke on 15.08.18.
+//  Copyright © 2018 Alexander Meinecke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
